1ba0cd04dd55ec12ecfbbe21bef99f1dd5575e7e
Bhagwat Rawat <bhagwat12rawat@gmail.com>
2022-07-26 22:16:40 +0530

    authentication

--- App.js
+++ App.js
@@ -9,23 +9,12 @@
 import React from 'react';
 import {Image, StatusBar, StyleSheet, Text, View} from 'react-native';
 import RootNavigator from './src/navigation';
-import Homescreen from './src/screens/homescreen';
-import DetailScreen from './src/screens/detailscreen';
-import DishDetailScreen from './src/screens/dishDetailScreen';
-import BasketScreen from './src/screens/basketScreen';
-import OrdersScreen from './src/screens/ordersScreen';
-import OrderDetailScreen from './src/screens/orderDetailScreen';
 import {NavigationContainer} from '@react-navigation/native';
+import {withAuthenticator} from 'aws-amplify-react-native';
+
 const App = () => {
   return (
     <NavigationContainer >
-      {/* our food item */}
-      {/* <Homescreen /> */}
-      {/* <DetailScreen /> */}
-      {/* <DishDetailScreen/> */}
-      {/* <BasketScreen/> */}
-      {/* <OrdersScreen/> */}
-      {/* <OrderDetailScreen/> */}
       <RootNavigator />
       <StatusBar
         hidden={false}
@@ -45,4 +34,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default App;
+export default withAuthenticator(App);
--- index.js
+++ index.js
@@ -5,5 +5,13 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import {Amplify} from 'aws-amplify';
+import config from './src/aws-exports';
 
+Amplify.configure({
+    ...config,
+    Analytics: {
+      disabled: true,
+    },
+  });
 AppRegistry.registerComponent(appName, () => App);
